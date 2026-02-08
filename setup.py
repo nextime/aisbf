56d1b65ec9a543bd9830083e4fe5bf82fb3fff88
Stefy Lanza <stefy@nexlab.net>
2026-02-08 09:07:09 +0200

    chore: Bump version to 0.3.3

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ class InstallCommand(_install):
 
 setup(
     name="aisbf",
-    version="0.3.2",
+    version="0.3.3",
     author="AISBF Contributors",
     author_email="stefy@nexlab.net",
     description="AISBF - AI Service Broker Framework || AI Should Be Free - A modular proxy server for managing multiple AI provider integrations",
